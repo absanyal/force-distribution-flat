fb5f270b33e3483e415154c6d1ba09bc25dbe636
Amit Bikram Sanyal <absanyal.ABS@gmail.com>
2024-09-27 13:17:52 -0400

    Refactor dump_filament function to use filament index instead of filament count

--- modules/makexyz.py
+++ modules/makexyz.py
@@ -4,7 +4,7 @@ from modules.filament import filament
 
 def dump_filament(filepath, filament_list):
     with open(filepath, 'w') as f:
-        fc = 0
+        filament_index = 0
         n_atoms = 0
         for fn in filament_list:
             n_atoms += fn.total_particles
@@ -12,17 +12,17 @@ def dump_filament(filepath, filament_list):
         f.write("{}\n".format(n_atoms))
         f.write("Properties=atom_types:I:1:molecule:I:1:pos:R:3:radius:R:1\n")
         for filamentname in filament_list:
-            fc += 1
-            print("Writing filament {} to {}".format(fc, filepath))
+            filament_index += 1
+            print("Writing filament {} to {}".format(filament_index, filepath))
             
             atom_type = "m"
             for layer in filamentname.layers:
                     for j in range(4):
-                        f.write("{} {} {:f} {:f} {:f} {}\n".format(atom_type, fc, layer.positions[j][0], layer.positions[j][1], layer.positions[j][2], filamentname.monomer_diameter/5))
+                        f.write("{} {} {:f} {:f} {:f} {}\n".format(atom_type, filament_index, layer.positions[j][0], layer.positions[j][1], layer.positions[j][2], filamentname.monomer_diameter/5))
             
             atom_type = "l"
             for linker_i, linker in enumerate(filamentname.linkers):
                 for atom_i in range(len(linker.positions)):
                     px, py, pz = linker.positions[atom_i]
                     atom_number = linker.indices[atom_i]
-                    f.write("{} {} {:f} {:f} {:f} {}\n".format(atom_type, fc, px, py, pz, filamentname.linker_diameter/5))
+                    f.write("{} {} {:f} {:f} {:f} {}\n".format(atom_type, filament_index, px, py, pz, filamentname.linker_diameter/5))
