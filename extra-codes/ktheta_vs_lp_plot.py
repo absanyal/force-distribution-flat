2b055de4b5037fd0371901370bcfc295c458c5e4
Amit Bikram Sanyal <absanyal.ABS@gmail.com>
2024-11-18 17:51:39 -0500

    Add curve fitting to ktheta vs lp and ktheta vs e2e plots for improved data analysis

--- extra-codes/ktheta_vs_lp_plot.py
+++ extra-codes/ktheta_vs_lp_plot.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import rcparams
+from scipy.optimize import curve_fit
+
+def f(x, m, c):
+    return m*x + c
 
 ktheta, lp, lp_err, e2e, e2e_err = np.loadtxt('ktheta_vs_lp.dat', unpack=True)
 
@@ -13,6 +17,16 @@ ax.plot(ktheta, lp, color='k', label='Data', marker='o', markersize=4)
 ax.set_xlabel(r'$k_{\theta}\,(k_BT)$')
 ax.set_ylabel(r'$l_p$ (nm)')
 
+popt, pcov = curve_fit(f, ktheta, lp)
+m, c = popt
+
+x_test = np.linspace(ktheta[0], ktheta[-1], 1000)
+y_test = f(x_test, m, c)
+
+ax.plot(x_test, y_test, color='b', ls = '--', label=r'$y = {:.4f}x + ({:.4f})$'.format(m, c))
+
+ax.legend()
+
 plt.savefig('ktheta_vs_lp.pdf')
 
 fig, ax = plt.subplots(constrained_layout=True, figsize=(6, 4))
@@ -23,5 +37,15 @@ ax.plot(ktheta, e2e, color='k', label='Data', marker='o', markersize=4)
 
 ax.set_xlabel(r'$k_{\theta}\,(k_BT)$')
 ax.set_ylabel(r'$\langle R_{{e-e}} \rangle$ (nm)')
+
+popt, pcov = curve_fit(f, ktheta, e2e)
+m, c = popt
+
+x_test = np.linspace(ktheta[0], ktheta[-1], 1000)
+y_test = f(x_test, m, c)
+
+# ax.plot(x_test, y_test, color='b', ls = '--', label=r'$y = {:.4f}x + ({:.4f})$'.format(m, c))
+
+ax.legend()
 
 plt.savefig('ktheta_vs_e2e.pdf')
