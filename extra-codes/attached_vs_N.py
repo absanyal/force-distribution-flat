bda9deab28be48ce6210ae15593159dac7748058
Amit Bikram Sanyal <absanyal.ABS@gmail.com>
2024-11-19 18:10:42 -0500

    Update attached_vs_N.py: extend N_list range, adjust Ebind0 and lp values, and refine plot labels for improved clarity

--- extra-codes/attached_vs_N.py
+++ extra-codes/attached_vs_N.py
@@ -20,10 +20,10 @@ n_input = n_input[1:]
 n_attach_input = n_attach_input[1:]
 fraction_attached = fraction_attached[1:]
 
-N_list = np.arange(4, 100)
+N_list = np.arange(4, 1000)
 average_length_list = []
 
-Ebind0 = -(4.8)
+Ebind0 = -(0.1)
 T = 1
 kB = 1
 beta = 1.0 / (kB * T)
@@ -31,7 +31,7 @@ R0 = 100
 Rb = 350
 r_mono = 2.5
 
-lp = 12000
+lp = 1200
 
 for N in N_list:
 
@@ -39,7 +39,7 @@ for N in N_list:
     p_list = np.zeros_like(n_list, dtype=float)
 
     for n_i, n in enumerate(n_list):
-        dEbend = r_mono * (n - 1) * kB * T * ( (1/Rb) - (1/R0)  )**2
+        dEbend = r_mono * (n - 1) * (kB * T * lp) * ( (1/Rb) - (1/R0)  )**2
         dEbind = n * Ebind0
         p_list[n_i] = (N - n + 1) * np.exp(-beta * (dEbend + dEbind))
 
@@ -66,7 +66,8 @@ max_val = np.max(average_length_list)
 
 plt.figure(figsize=(6, 4), dpi=300, constrained_layout=True)
 
-# plt.plot(N_list, average_length_list, color='black')
+plt.plot(N_list, average_length_list, color='black')
+
 
 plt.xlabel('Number of monomers')
 plt.ylabel('Average length attached')
@@ -80,14 +81,14 @@ plt.ylabel('Average length attached')
 
 # plt.axhline(max_val, color='red', linestyle='--', linewidth = 0.5, label='Max: {:.2f}'.format(max_val))
 
-plt.axhline(data_average, color='green', linestyle='--', linewidth = 0.5, label='Data max: {:.2f}'.format(data_average))
+# plt.axhline(data_average, color='green', linestyle='--', linewidth = 0.5, label='Data max: {:.2f}'.format(data_average))
 
-plt.plot(n_input, n_attach_input, 'o', color='blue', label='Data')
+# plt.plot(n_input, n_attach_input, 'o', color='blue', label='Data')
 
 # plt.plot(n_input, fraction_attached, 'o', color='blue', label='Data')
 
-plt.title(r'$l_p = {:.2f}\,\mu m$'.format(lp))
+plt.title(r'$l_p = {:.2f}$ nm'.format(lp))
 
-plt.legend()
+# plt.legend()
 
 plt.savefig('attached_length_vs_N.pdf')
