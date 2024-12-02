00f86e76cfbbf3481f9cadb679c252c4ddb36e0b
Amit Bikram Sanyal <absanyal.ABS@gmail.com>
2024-12-02 14:33:01 -0500

    Refactor attached_vs_N_fit.py to improve variable naming and calculation logic for better clarity and accuracy

--- extra-codes/attached_vs_N_fit.py
+++ extra-codes/attached_vs_N_fit.py
@@ -15,10 +15,6 @@ for n, n_attach in zip(n_input, n_attach_input):
         counter += 1
 
 data_average = data_average / counter
-        
-n_input = n_input[1:]
-n_attach_input = n_attach_input[1:]
-fraction_attached = fraction_attached[1:]
 
 n_input = np.array(n_input)
 n_attach_input = np.array(n_attach_input)
@@ -30,17 +26,16 @@ Rb = 350
 R0 = 100
 r_mono = 2.5
 T = 1
+kB = 1
 
-def average_length(N_max, Bs, Ebind0):
+def average_length(N_max, lp, Ebind0):
     n_list = np.arange(1, N_max+1)
     p_list = np.zeros_like(n_list, dtype=float)
     
-    p_list = np.zeros_like(n_list, dtype=float)
-
-    beta = 1.0 / T
+    beta = 1.0 / (kB * T)
 
     for n_i, n in enumerate(n_list):
-        dEbend = r_mono * (n - 1) * Bs * ( (1/Rb) - (1/R0)  )**2
+        dEbend = r_mono * (n - 1) * (lp * kB * T) * ( (1/Rb) - (1/R0)  )**2
         dEbind = n * Ebind0
         p_list[n_i] = (N_max - n + 1) * np.exp(-beta * (dEbend + dEbind))
 
@@ -54,26 +49,23 @@ def average_length(N_max, Bs, Ebind0):
 
 average_length_vec = np.vectorize(average_length)
 
-popt, pcov = curve_fit(average_length_vec, n_input, n_attach_input, p0=[5000, -1], maxfev=100000,    bounds=([0, -10], [100000, 0]))
+popt, pcov = curve_fit(lambda N_max, lp, Ebind0: average_length_vec(N_max, lp, Ebind0), n_input, n_attach_input, p0=[1200, -1.0], bounds=([0, -np.inf], [np.inf, 0]))
 
-average_length_fit = average_length_vec(n_input, *popt)
-
-Bs, Ebind0 = popt
-err_Bs, err_Ebind0 = np.sqrt(np.diag(pcov))
-print("Bs = {:.2f} +/- {:.2f}".format(Bs, err_Bs))
+lp, Ebind0 = popt
+err_lp, err_Ebind0 = np.sqrt(np.diag(pcov))
+print("lp = {:.2f} +/- {:.2f}".format(lp, err_lp))
 print("Ebind0 = {:.2f} +/- {:.2f}".format(Ebind0, err_Ebind0))
+
+average_length_fit = average_length_vec(n_input, lp, Ebind0)
 
 plt.figure(figsize=(6, 4), dpi=300, constrained_layout=True)
 
 plt.plot(n_input, n_attach_input, 'o', color='blue', label='Data')
-plt.plot(n_input, average_length_fit, color='red', label='Fit: Bs = {:.2f}, Ebind0 = {:.2f}'.format(Bs, Ebind0))
+plt.plot(n_input, average_length_fit, color='red', label=r'Fit:\\$l_p = {:.2f} \pm {:.2f}$\\$E_{{\mathrm{{bind}}}}^0 = {:.2f} \pm {:.2f}$'.format(lp, err_lp, Ebind0, err_Ebind0))
 
 plt.xlabel('Number of monomers')
-plt.ylabel('Number of attached monomers')
+plt.ylabel('Number of attached monomers', fontsize=16)
 
 plt.legend()
 
 plt.savefig('attached_vs_N_fit.pdf')
-
-test = average_length_vec(100, 38650, -4.8)
-print(test)
