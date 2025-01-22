a3b4f2beacc9eb76921b7d63fe1e644992459164
Amit Bikram Sanyal <absanyal.ABS@gmail.com>
2025-01-22 13:02:03 -0500

    Enhance r_hist_plot.py by improving histogram plotting with constrained layout, adjusting histogram parameters, and adding expected distribution line; update axis limits and legend formatting.

--- r_hist_plot.py
+++ r_hist_plot.py
@@ -9,22 +9,28 @@ r0 = 1.8028
 r_min = 1.6326
 r_max = 2.1251
 
+r_list_file, p_list_file = np.loadtxt('extra-codes/expected_pr.dat', unpack=True)
+
 s_list = np.loadtxt('r_histogram.dat')
 
 average_r = np.mean(s_list)
 
-plt.figure(figsize=(5, 5))
+plt.figure(figsize=(5, 5), constrained_layout=True)
 
-plt.hist(s_list, bins=100, density=True, label=r'$\Delta\,s_{\mathrm{linker}}$', color='b', alpha=0.5, edgecolor='None', histtype='stepfilled', rwidth=0.8)
+plt.hist(s_list, bins='auto', density=True, label=r'$\Delta\,s_{\mathrm{linker}}$ from LAMMPS', color='b', edgecolor='b', histtype='step', rwidth=1, lw=2.0)
 
-plt.axvline(x=r0, color='r', linestyle='--', label=r'$r_0 = {:.4f}$ nm'.format(r0))
-plt.axvline(x=r_min, color='g', linestyle='--', label=r'$r_{{\mathrm{{min}}}} = {:.4f}$ nm'.format(r_min))
-plt.axvline(x=r_max, color='g', linestyle='--', label=r'$r_{{\mathrm{{max}}}} = {:.4f}$ nm'.format(r_max))
-plt.axvline(x=average_r, color='b', linestyle='--', label=r'$\langle\Delta\,s_{{\mathrm{{linker}}}}\rangle = {:.4f}$ nm'.format(average_r))
+plt.axvline(x=r0, color='r', linestyle='--', label=r'$r_0 = {:.4f}$ nm'.format(r0), lw=1.0)
+plt.axvline(x=r_min, color='g', linestyle='--', label=r'$r_{{\mathrm{{min}}}} = {:.4f}$ nm'.format(r_min), lw=1.0)
+plt.axvline(x=r_max, color='g', linestyle='--', label=r'$r_{{\mathrm{{max}}}} = {:.4f}$ nm'.format(r_max), lw=1.0)
+plt.axvline(x=average_r, color='b', linestyle='--', label=r'$\langle\Delta\,s_{{\mathrm{{linker}}}}\rangle = {:.4f}$ nm'.format(average_r), lw=1.0)
+
+plt.plot(r_list_file, p_list_file, '-', color='k', label='Expected distribution')
 
 plt.xlabel(r'$\Delta\,s_{{\mathrm{{linker}}}}$ (nm)')
 plt.ylabel('Probability density')
 
-plt.legend()
+plt.xlim(left = 1, right = max(r_list_file))
+
+plt.legend(fontsize=10)
 
 plt.savefig('plots/r_hist.pdf')
