d4a2ee8592c9c85bbd5fd3c722a0e3fca6c791f8
Amit Bikram Sanyal <absanyal.ABS@gmail.com>
2025-01-22 13:37:17 -0500

    Add submit_single.sh script for LAMMPS simulation setup and execution

---
+++ submit_single.sh
@@ -0,0 +1,23 @@
+nres=1
+
+mkdir -p plots
+mkdir -p data
+mkdir -p dump
+mkdir -p com_pos
+mkdir -p link_pos
+mkdir -p mon_pos
+mkdir -p info
+mkdir -p thermo
+
+echo "variable xx uloop $nres" > in.variables
+echo >> in.variables
+printf  "variable vseed universe " >> in.variables
+for (( x=0; x<$nres; x++ ))
+do
+    printf "%i " $RANDOM >> in.variables
+
+done
+
+time mpirun -n 16 lmp_mpi -in input_single.lammps > out.run
+
+rm log.*
