1cade69eb389c7b362dd74edfd22d4f15537079c
Amit Bikram Sanyal <absanyal.ABS@gmail.com>
2025-05-01 12:36:17 -0400

    Update output message for bending energy calculation in measure_Eb.py

--- measure_Eb.py
+++ measure_Eb.py
@@ -102,4 +102,4 @@ E_bend = final_total_energy - initial_total_energy
 
 print("R0 = {:.2f}".format(R))
 print
-print("E_bend = {:.4f} kBT".format(E_bend / kBT0))
+print("delta E_bend = {:.4f} kBT".format(E_bend / kBT0))
