3b2d3108c0ad20a171f4a50d9bacbab09bbdcc9a
Amit Bikram Sanyal <absanyal.ABS@gmail.com>
2025-05-02 17:33:46 -0400

    Add correlation analysis and plotting scripts for persistence length evaluation

---
+++ lp_analysis/correlations.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+raw_data = np.loadtxt('../mon_pos/mon_pos.1.txt')
+
+num_time_steps = raw_data.shape[0]
+print("Number of time steps: {}".format(num_time_steps))
+
+fraction_of_recording_to_skip = 0.1
+t_start_index = int(num_time_steps * fraction_of_recording_to_skip)
+print("Starting averaging after {} time steps".format(t_start_index))
+
+t_list = np.arange(1, num_time_steps + 1)
+# print(t_list)
+
+t_list = t_list[t_start_index:]
+num_time_steps = len(t_list)
+print("Number of time steps after skipping: {}".format(num_time_steps))
+
+num_atoms = int((raw_data.shape[1] - 1) / 3)
+print("Number of atoms: {}".format(num_atoms))
+
+num_bond_vectors = num_atoms - 1
+print("Number of bond vectors: {}".format(num_bond_vectors))
+
+processed_data = np.zeros((num_time_steps, num_bond_vectors, 3))
+
+for t_i in range(num_time_steps):
+    row = raw_data[t_i, 1:]
+    for a_i in range(num_atoms - 1):
+        atom_1_x = row[a_i * 3]
+        atom_1_y = row[a_i * 3 + 1]
+        atom_1_z = row[a_i * 3 + 2]
+        atom_2_x = row[(a_i + 1) * 3]
+        atom_2_y = row[(a_i + 1) * 3 + 1]
+        atom_2_z = row[(a_i + 1) * 3 + 2]
+        bond_vector_x = atom_2_x - atom_1_x
+        bond_vector_y = atom_2_y - atom_1_y
+        bond_vector_z = atom_2_z - atom_1_z
+        bond_vector = [bond_vector_x, bond_vector_y, bond_vector_z]
+        bond_vector = np.array(bond_vector)
+        
+        processed_data[t_i, a_i] = bond_vector
+        
+
+s_list = np.arange(0, num_bond_vectors)
+# print("s_list: {}".format(s_list))
+
+time_averaged_correlations = np.zeros(len(s_list))
+
+max_t = np.max(t_list)
+t_skip = np.round(0.1 * max_t, 0)
+
+for t_i in range(num_time_steps):
+    percentage = (t_i + 1) / num_time_steps
+    if t_i % t_skip == 0 or t_i == num_time_steps - 1:
+        print("Processing step {}/{} | {:.0f}%".format(t_i + 1, num_time_steps, percentage * 100))
+    for b_i in range(num_bond_vectors):
+        delta_s_range = np.arange(num_bond_vectors - b_i)
+        # print("For bond vector {}, correlation will be calculated for bonds:".format(b_i))
+        # for delta_s in delta_s_range:
+        #     print("Bond vector {}".format(b_i + delta_s))
+        for delta_s in delta_s_range:
+            v1 = processed_data[t_i, b_i]
+            v2 = processed_data[t_i, b_i + delta_s]
+            
+            v_1_norm = np.linalg.norm(v1)
+            v_2_norm = np.linalg.norm(v2)
+
+            corr = np.dot(v1, v2) / (v_1_norm * v_2_norm)
+            
+            time_averaged_correlations[delta_s] += corr / (num_bond_vectors - delta_s)
+
+# Normalize the time-averaged correlations
+time_averaged_correlations /= num_time_steps
+
+with open("lp_data_linkers.dat", 'w') as f:
+    f.write("# s \t C(s)\n")
+    for s_i in range(len(s_list)):
+        f.write("{} \t {}\n".format(s_list[s_i], time_averaged_correlations[s_i]))
---
+++ lp_analysis/plot_lp.py
@@ -0,0 +1,101 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import rcparams
+
+from scipy.signal import find_peaks
+from scipy.optimize import curve_fit
+
+def envelope(s, lp):
+    """
+    Calculate the envelope of the correlation function.
+    """
+    return np.exp(-s / lp)
+
+def linear_envelope(s, lp):
+    """
+    Linear fit function for the log of the envelope
+    """
+    return -s / lp
+
+s, corr_s = np.loadtxt('lp_data_linkers.dat', unpack=True)
+
+L = len(s) + 1
+print("L: {}".format(L))
+
+# max_s = 60
+# max_s = int(s[-1] * 0.5)
+max_s = max(s) 
+print("Max s: {}".format(max_s))
+
+s = s[s < max_s]
+max_index = len(s)
+corr_s = corr_s[:max_index]
+
+peaks = find_peaks(corr_s)
+peak_indices = peaks[0]
+
+peak_indices = np.insert(peak_indices, 0, 0)
+
+if len(peak_indices) > 1:
+    print("Fitting using {} peaks".format(len(peak_indices)))
+
+    peak_heights = [corr_s[i] for i in peak_indices]
+    peak_heights = np.array(peak_heights)
+
+    log_peaks = np.log(peak_heights)
+
+# Fit the envelope to the correlation function
+if len(peak_indices) > 1:
+    popt, pcov = curve_fit(envelope, s[peak_indices], peak_heights)
+
+    lp = popt[0]
+    lp_err = np.sqrt(np.diag(pcov))[0]
+    print("lp: {:.2f} +/- {:.2f}".format(lp, lp_err))
+
+    y_fit = envelope(s, lp)
+
+    # Fit the linear envelope to the log of the correlation function
+    popt_log, pcov_log = curve_fit(linear_envelope, s[peak_indices], log_peaks)
+    lp_log = popt_log[0]
+    lp_log_err = np.sqrt(np.diag(pcov_log))[0]
+    print("lp_log: {:.2f} +/- {:.2f}".format(lp_log, lp_log_err))
+
+    y_fit_log = np.exp(linear_envelope(s, lp_log))
+
+#############################################################################################
+
+if len(peak_indices) > 1:
+    L_by_lp = L / lp
+    print("L/lp: {:.2f}".format(L_by_lp))
+
+    L_by_lp_log = L / lp_log
+    print("L/lp_log: {:.2f}".format(L_by_lp_log))
+
+###########################################################################################
+
+plt.figure(figsize=(6, 6), constrained_layout=True)
+
+plt.plot(s, corr_s, 'o-', markersize=2, color='k', label='Correlation function', lw=0.5)
+
+if len(peak_indices) > 1:
+    for i in peak_indices:
+        plt.scatter(s[i], corr_s[i], color='red', s=50, marker='x')
+
+    plt.plot(s, y_fit, 'b--', label=r'$l_p$ (exponential fit) = ${:.2f}$ $\pm$ ${:.2f}$\\ $L/l_p = {:.2f}$'.format(lp, lp_err, L_by_lp))
+
+    plt.plot(s, y_fit_log, 'r--', label=r'$l_p$ (linear fit) = ${:.2f}$ $\pm$ ${:.2f}$\\ $L/l_p = {:.2f}$'.format(lp_log, lp_log_err, L_by_lp_log))
+
+plt.xlabel(r'$s$')
+# plt.ylabel(r'$C(s)$')
+# plt.ylabel(r'$ \frac{1}{N-1} \sum \limits_{i} \langle \vec{\tau}_{i} \cdot \vec{\tau}_{i+s} \rangle$')
+plt.ylabel('Correlation function')
+
+plt.axhline(0, color='k', lw=0.5, ls='--', alpha=0.5)
+plt.axvline(0, color='k', lw=0.5, ls='--', alpha=0.5)
+
+# plt.yscale('log')
+# plt.ylim(bottom=1e-4)
+
+plt.legend()
+
+plt.savefig('persistence_linkers.pdf', bbox_inches='tight')
